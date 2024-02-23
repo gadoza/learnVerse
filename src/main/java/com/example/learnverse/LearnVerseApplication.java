8ae3095e99c9139710b0b7c3a44958721a6939f7
Mohamed Gad <mohamed.gad@wscdev.net>
2024-02-23 16:14:54 +0200

    Initial commit

---
+++ src/main/java/com/example/learnverse/LearnVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnVerseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnverse/LearnVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

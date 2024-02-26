16db97f7556673aec3f8f7c49818d5d18b489126
Mohamed Gad <mohamed.gad@wscdev.net>
2024-02-26 23:36:38 +0200

    customise our own security

--- src/main/java/com/example/learnverse/config/WebSecurityConfig.java
+++ src/main/java/com/example/learnverse/config/WebSecurityConfig.java
@@ -1,15 +1,32 @@
 package com.example.learnverse.config;
 
+import com.example.learnverse.config.security.filters.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .addFilterAt(customAuthenticationFilter , UsernamePasswordAuthenticationFilter.class) // where to put our filter the place that holds the userName and password
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
